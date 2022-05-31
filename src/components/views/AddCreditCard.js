70ef7051b6362705c1b39b4ace0343c6b2bc4781
Edson Lima Costa <ecosta.vermont@alelo.com.br>
2022-05-31 20:28:00 -0300

    rebase complete

--- src/components/views/AddCreditCard.js
+++ src/components/views/AddCreditCard.js
@@ -1,56 +1,70 @@
-const AddCreditCard = () => {
+import React, { Component } from 'react';
+import Form from "react-validation/build/form";
+import Input from "react-validation/build/input";
+export default class AddCreditCard extends Component {
+    constructor(props) {
+        super(props);
 
-    return (
-        <div class="col-3">
-            <div class="card shadow mb-5 bg-body rounded">
-                <div class="p-3 rounded-top header-form">Adicionar cartão de crédito</div>
-                    <div class="header-form-childre p-4">
-                        <form data-toggle="validator">
-                            <div class="mb-3">
-                                <input
-                                type="number"
-                                class="form-control"
-                                id="card-number"
-                                name="card-number"
-                                placeholder="Número do cartão"
-                                required/>
-                            </div>
-                            <div class="mb-3">
-                                <input
-                                type="text"
-                                class="form-control"
-                                id="card-name"
-                                name="card-name"
-                                placeholder="Nome no cartão"
-                                required/>
-                            </div>
-                            <div class="mb-1">
-                                <span class="float-start">    
-                                    <input
-                                    type="date"
-                                    class="form-control"
-                                    id="date-valid"
-                                    name="date-valid"
-                                    placeholder="Data de validade"
-                                    required/>
-                                </span>
-                                <span class="float-end"><span>
+        this.state = {
+            cardNumber: '',
+            printedName: '',
+            expirationTime:'',
+            securityCode: ''
+        };
+
+    }
+
+    render() {
+        return (
+            <div className="col-3">
+                <div className="card shadow mb-5 bg-body rounded">
+                    <div className="p-3 rounded-top header-form">Adicionar cartão de crédito</div>
+                        <div className="header-form-childre p-4">
+                            <form data-toggle="validator">
+                                <div className="mb-3">
                                     <input
                                     type="number"
-                                    class="form-control"
-                                    id="cvv"
-                                    name="cvv"
-                                    placeholder="CVV"
-                                    required/></span></span>
-                            </div>
-                        </form>
-                    </div>
-            <div class="d-grid m-3">
-                <input type="submit" class="btn-submit-form" value="Adicionar Cartão" />
+                                    className="form-control"
+                                    id="card-number"
+                                    name="card-number"
+                                    placeholder="Número do cartão"
+                                    required/>
+                                </div>
+                                <div className="mb-3">
+                                    <input
+                                    type="text"
+                                    className="form-control"
+                                    id="card-name"
+                                    name="card-name"
+                                    placeholder="Nome no cartão"
+                                    required/>
+                                </div>
+                                <div className="mb-1">
+                                    <span className="float-start">    
+                                        <input
+                                        type="date"
+                                        className="form-control"
+                                        id="date-valid"
+                                        name="date-valid"
+                                        placeholder="Data de validade"
+                                        required/>
+                                    </span>
+                                    <span className="float-end"><span>
+                                        <input
+                                        type="number"
+                                        className="form-control"
+                                        id="cvv"
+                                        name="cvv"
+                                        placeholder="CVV"
+                                        required/></span></span>
+                                </div>
+                            </form>
+                        </div>
+                <div className="d-grid m-3">
+                    <input type="submit" className="btn-submit-form" value="Adicionar Cartão" />
+                </div>
             </div>
         </div>
-      </div>
     );
+}
 };
-
-export default AddCreditCard;
