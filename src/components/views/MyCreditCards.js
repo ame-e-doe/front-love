6738b2ea049a6a97964f8510ab6437b9fbbcbcf9
rennan.costa <rennan.costa@pcivil.rj.gov.br>
2022-05-31 21:39:52 -0300

    listagem de cartao no cart

--- src/components/views/MyCreditCards.js
+++ src/components/views/MyCreditCards.js
@@ -1,24 +1,29 @@
-function MyCreditCards() {
+function MyCreditCards({cardNumber, printedName, expirationDate}) {
 
-    return (
+  let numberFormated = cardNumber;
+  if (cardNumber) {
+    numberFormated = cardNumber.substring(11,15)
+  }
+
+  return (
         <div>
           <div className="card shadow mb-5 bg-body rounded-2">
             <div className="p-3 rounded-top header-form">Selecione o cartão</div>
             <div className="p-3 container-conteudo">
                 <div>
-                    <div className="d-flex justify-content-center mb-3" style={{alignItems: "center"}}>
-                        <span className="px-3" style={{textAlign: "center"}}>
+                    <div className="d-flex justify-content-center" style={{textAlign: "center"}}>
+                        <span className="px-2">
                           <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"/>
                         </span>
-                        <span className="px-3" style={{textAlign: "center"}}>Cartão terminado em 7888</span>
-                        <span className="px-3" style={{textAlign: "center"}}>Rennan S. Costa</span>
-                        <span className="px-3" style={{textAlign: "center"}}>09/2029</span>
+                        <span className="px-2" style={{textAlign: "center"}}>Cartão terminado em {numberFormated}</span>
+                        <span className="px-2" style={{textAlign: "center"}}>{printedName}</span>
+                        <span className="px-2" style={{textAlign: "center"}}>{expirationDate}</span>
                     </div>
                 </div>
             </div>
           </div>
       </div>
-    );
+  );
 }
 
 export default MyCreditCards;
